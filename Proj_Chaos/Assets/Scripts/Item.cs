33271f292ed5b1b8f1f48f994158e350e87cf4d8
Jane <glazychevaeo@gmail.com>
2019-10-21 16:55:42 -0400

    Item database system

---
+++ Proj_Chaos/Assets/Scripts/Item.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item
+{
+    public int itemId;
+    public string itemName;
+    public GameObject itemPrefab;
+
+    public Item(ItemSO item)
+    {
+        itemId = item.itemId;
+        itemName = item.itemName;
+        itemPrefab = item.itemPrefab;
+    }
+}
---
+++ Proj_Chaos/Assets/Scripts/ItemSO.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSO : MonoBehaviour
+{
+    public int itemId;
+    public string itemName;
+    public GameObject itemPrefab;
+}
