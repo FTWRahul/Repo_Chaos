5e6c1810068e759889e78978673edb411fbcd456
Jane <glazychevaeo@gmail.com>
2019-10-21 18:16:44 -0400

    Assets

--- Proj_Chaos/Assets/Scripts/ItemSO.cs
+++ Proj_Chaos/Assets/Scripts/ItemSO.cs
@@ -1,10 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class ItemSO : MonoBehaviour
+public enum ItemSize
+{
+        SMALL,
+        MEDIUM,
+        LARGE
+}
+
+[CreateAssetMenu(menuName = "Items", fileName = "newItem")]
+public class ItemSO : ScriptableObject
 {
     public int itemId;
     public string itemName;
     public GameObject itemPrefab;
+    public ItemSize itemSize;
+
 }
