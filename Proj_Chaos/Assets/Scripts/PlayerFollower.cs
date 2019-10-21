3e701ae9d1f326701132fc5c849bc5f8e8b5898d
CubeMD <markelovdp@gmail.com>
2019-10-21 19:19:41 -0400

    PlayerMovement

---
+++ Proj_Chaos/Assets/Scripts/PlayerFollower.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollower : MonoBehaviour
+{
+    public Transform target;
+
+    private void LateUpdate()
+    {
+        transform.position = target.TransformPoint(new Vector3(0, 2, -5));
+        transform.rotation = Quaternion.Euler(20, target.eulerAngles.y, 0);
+    }
+}
---
+++ Proj_Chaos/Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+    public float mouseSensitivity;
+    public float moveSpeed;
+    public CharacterController cc;
+    
+    private void Update()
+    {
+        transform.Rotate(0, Input.GetAxis("Mouse X") * Time.deltaTime * mouseSensitivity, 0);
+        
+        Vector3 dir = new Vector3(Input.GetAxisRaw("Horizontal"), 0,Input.GetAxisRaw("Vertical")).normalized;
+        
+        cc.Move((transform.rotation * dir) * moveSpeed * Time.deltaTime);
+    }
+}
