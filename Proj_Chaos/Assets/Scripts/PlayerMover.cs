9d67a4cafbb7d7c1def09a093a2e96358132efce
CubeMD <markelovdp@gmail.com>
2019-10-22 12:10:02 -0400

    Slapping and prefabs

--- Proj_Chaos/Assets/Scripts/PlayerMover.cs
+++ Proj_Chaos/Assets/Scripts/PlayerMover.cs
@@ -7,14 +7,21 @@ public class PlayerMover : MonoBehaviour
 {
     public float mouseSensitivity;
     public float moveSpeed;
-    public CharacterController cc;
-    
+    private CharacterController _cc;
+
+    private void Start()
+    {
+        _cc = GetComponent<CharacterController>();
+    }
+
     private void Update()
     {
         transform.Rotate(0, Input.GetAxis("Mouse X") * Time.deltaTime * mouseSensitivity, 0);
         
-        Vector3 dir = new Vector3(Input.GetAxisRaw("Horizontal"), 0,Input.GetAxisRaw("Vertical")).normalized;
+        Vector3 inputDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0,Input.GetAxisRaw("Vertical")).normalized;
+
+        Vector3 moveDir = transform.rotation * inputDir + Physics.gravity;
         
-        cc.Move((transform.rotation * dir) * moveSpeed * Time.deltaTime);
+        _cc.Move(moveSpeed * Time.deltaTime * moveDir);
     }
 }
---
+++ Proj_Chaos/Assets/Scripts/Slapper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Slapper : MonoBehaviour
+{
+    public float radius;
+    public float timeBetweenSlaps;
+    public float timeIncapacitated;
+    private Collider[] _overlappedObjects;
+
+    private void Slap()
+    {
+        Physics.OverlapSphereNonAlloc(transform.position, radius, _overlappedObjects);
+
+        foreach (Collider col in _overlappedObjects)
+        {
+            Slapper slapper = col.GetComponent<Slapper>();
+
+            if (slapper != null)
+            { 
+                slapper.Slapped(); 
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Slap();
+        }
+    }
+
+    public void Slapped()
+    {
+        
+    }
+}
