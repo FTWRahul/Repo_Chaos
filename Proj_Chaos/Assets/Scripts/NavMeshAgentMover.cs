ffc5f7d34290d26a6236feeb87e632145ad7eb0d
CubeMD <markelovdp@gmail.com>
2019-10-22 11:17:43 -0400

    nav mesh testing

---
+++ Proj_Chaos/Assets/Scripts/NavMeshAgentMover.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshAgentMover : MonoBehaviour
+{
+
+    private void Start()
+    {
+        GetComponent<NavMeshAgent>().SetDestination(transform.position - new Vector3(45, 0, 0));
+    }
+}
