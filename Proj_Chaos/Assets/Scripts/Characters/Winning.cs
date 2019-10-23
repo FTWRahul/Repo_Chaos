4b1d4861463a45e08798f165414c70429c7bc0ea
FTWRahul <r.poundarik@gmail.com>
2019-10-23 15:53:28 -0400

    Lost condition added

--- Proj_Chaos/Assets/Scripts/Characters/Winning.cs
+++ Proj_Chaos/Assets/Scripts/Characters/Winning.cs
@@ -6,9 +6,13 @@ using TMPro;
 
 public class Winning : MonoBehaviour
 {
+    public QuestGenerator player;
     public void OnEnable()
     {
-        FindObjectOfType<ExitCheck>().OnItemRemoved.AddListener(CheckIfWon);
+        ExitCheck exit = FindObjectOfType<ExitCheck>();
+        player = FindObjectOfType<CharacterController>().GetComponent<QuestGenerator>();
+        exit.OnRightItem.AddListener(CheckIfWon);
+        exit.OnItemRemoved.AddListener(CheckIfLost);
     }
 
     public void CheckIfWon()
@@ -19,4 +23,18 @@ public class Winning : MonoBehaviour
             UIManager.Instance.endMenu.text.text = "You win!";
         }
     }
+    public void CheckIfLost()
+    {
+        for (int i = 0; i < player.itemsToCollect.Count; i++)
+        {
+            if (ItemsDatabase.Instance.objectsInScene[player.itemsToCollect[i]] < 1)
+            {
+                GameManager.Instance.UpdateState(GameManager.GameState.END);
+                UIManager.Instance.endMenu.text.text = "You Loose ;_;";
+
+            }
+        }
+    }
+    
+
 }
