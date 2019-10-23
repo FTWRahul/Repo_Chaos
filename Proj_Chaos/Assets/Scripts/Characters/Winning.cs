239b075dfdccd62ee4546bf7f2995c5c874f31ec
CubeMD <markelovdp@gmail.com>
2019-10-23 15:14:15 -0400

    something

---
+++ Proj_Chaos/Assets/Scripts/Characters/Winning.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Winning : MonoBehaviour
+{
+    public void OnEnable()
+    {
+        FindObjectOfType<ExitCheck>().OnItemRemoved.AddListener(CheckIfWon);
+    }
+
+    public void CheckIfWon()
+    {
+        if (PaperListMenu.Instance.TextOrderedList.TrueForAll(x => x.fontStyle == FontStyles.Strikethrough))
+        {
+            GameManager.Instance.UpdateState(GameManager.GameState.END);
+        }
+    }
+}
