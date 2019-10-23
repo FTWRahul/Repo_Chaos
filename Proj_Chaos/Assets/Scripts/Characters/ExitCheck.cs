3a41bd6abe3f48161a4617cc41f5a55a7f832676
FTWRahul <r.poundarik@gmail.com>
2019-10-23 12:23:38 -0400

    Strickthrough Works

--- Proj_Chaos/Assets/Scripts/Characters/ExitCheck.cs
+++ Proj_Chaos/Assets/Scripts/Characters/ExitCheck.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using TMPro;
 
 public class ExitCheck : MonoBehaviour
 {
+    public Events.EventWrongItem OnWrongItem;
+    public Events.EventRightItem OnRightItem;
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<AgentBehaviour>() != null)
@@ -20,9 +20,35 @@ public class ExitCheck : MonoBehaviour
         {
             if (other.GetComponent<PickUp>().itemHold != null)
             {
+                bool isRightItem = false;
+                ItemId item =  other.GetComponent<PickUp>().itemHold;
+                for (int i = 0; i < other.GetComponent<QuestGenerator>().itemsToCollect.Count; i++)
+                {
+                    if (item.itemId == other.GetComponent<QuestGenerator>().itemsToCollect[i])
+                    {
+                        PaperListMenu.Instance.TextOrderedList[i].fontStyle = FontStyles.Strikethrough;
+                        // Do the thing on UI
+                        
+                        // Play the right sound
+                        isRightItem = true;
+                        break;
+                    }
+                    else
+                    {
+                        isRightItem = false;
+                    }
+                }
+
+                if (isRightItem)
+                {
+                    OnRightItem.Invoke();
+                }
+                else
+                {
+                    OnWrongItem.Invoke();
+                }
                 
-               ItemId item =  other.GetComponent<PickUp>().itemHold;
-               int itemID = item.itemId;
+                int itemID = item.itemId;
                ItemsDatabase.Instance.objectsInScene[itemID]--;
                other.GetComponent<PickUp>().DropItem();
                item.Vanish();
