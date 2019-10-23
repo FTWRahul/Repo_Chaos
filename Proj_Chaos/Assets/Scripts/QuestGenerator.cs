0e525c8fd0a23dc7c8736e246f450e97053d7a6f
Jane <glazychevaeo@gmail.com>
2019-10-22 21:30:19 -0400

    NPC behaviour

--- Proj_Chaos/Assets/Scripts/QuestGenerator.cs
+++ Proj_Chaos/Assets/Scripts/QuestGenerator.cs
@@ -6,7 +6,7 @@ using Random = UnityEngine.Random;
 
 public class QuestGenerator : MonoBehaviour
 {
-    private List<int> _itemsToCollect = new List<int>();
+    public List<int> itemsToCollect = new List<int>();
     public int maxItemAmount;
 
     private void Start()
@@ -14,7 +14,7 @@ public class QuestGenerator : MonoBehaviour
         for (int i = 0; i < maxItemAmount; i++)
         {
             int rand = Random.Range(0, ItemsDatabase.Instance.database.Count);
-            _itemsToCollect.Add(ItemsDatabase.Instance.database[rand].itemId);
+            itemsToCollect.Add(ItemsDatabase.Instance.database[rand].itemId);
         }
     }
 }
