46e2c282b542e1075cdac315dd98bf70d55a481d
Jane <glazychevaeo@gmail.com>
2019-10-23 00:28:55 -0400

    Folders and anim

---
+++ Proj_Chaos/Assets/Scripts/Characters/AgentBehaviour.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Random = UnityEngine.Random;
+
+public class AgentBehaviour : MonoBehaviour
+{
+    public Events.EventEPressed OnEPressed;
+    public Events.EventLMBPressed OnLMBPressed;
+    
+    [SerializeField] private List<Transform> possibleSections = new List<Transform>();
+    [SerializeField] private Transform exit;
+    [SerializeField] private float probability;
+    
+    private Vector3 _dest;
+    private int _itemToCollect;
+    private NavMeshAgent _navMeshAgent;
+    private QuestGenerator _questGenerator;
+    private PickUp _pickUp;
+    private Slapper _slapper;
+
+    private void Start()
+    {
+        _slapper = GetComponent<Slapper>();
+        _slapper.OnCharacterEndSlap.AddListener(CheckForItem);
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        _questGenerator = GetComponent<QuestGenerator>();
+        _pickUp = GetComponent<PickUp>();
+        _itemToCollect = _questGenerator.itemsToCollect[0];
+        
+        InitSections();
+    }
+
+    void InitSections()
+    {
+        TypeSize goalSectionType = ItemsDatabase.Instance.database[_itemToCollect].typeSize;
+
+        foreach (Section section in MapFactory.spawnnedSections)
+        {
+            if (section.typeSize == goalSectionType)
+            {
+                possibleSections.Add(section.transform);
+            }
+        }
+    }
+
+    void ChooseNewSection()
+    {
+        if (possibleSections.Count > 0)
+        {
+            int random = Random.Range(0, possibleSections.Count);
+            _dest = possibleSections[random].position;
+            possibleSections.RemoveAt(random);
+        }
+        else
+        {
+            _dest = exit.position;
+        }
+    }
+
+    private void Update()
+    {
+        if (_navMeshAgent.pathStatus == NavMeshPathStatus.PathComplete)
+        {
+            CheckForItem();
+        }
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    { 
+        if( other.GetComponent<Slapper>())
+        {
+            if (Random.value <= probability)
+            {
+                OnLMBPressed.Invoke();
+
+                StartCoroutine(WaitForAction());
+            }
+        }
+    }
+
+    private IEnumerator WaitForAction()
+    {
+        yield return new WaitUntil(() => _pickUp.canDoAction == true);
+
+        CheckForItem();
+    }
+
+    private void CheckForItem()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, 5);
+
+        foreach (Collider collider in colliders)
+        {
+            if (collider.GetComponent<ItemId>() && collider.GetComponent<ItemId>().itemId == _itemToCollect)
+            {
+                OnEPressed.Invoke();
+
+                _dest = _pickUp.hasItem ? exit.position : collider.transform.position;
+                break;
+            }
+            else
+            {
+                if (_navMeshAgent.pathStatus == NavMeshPathStatus.PathComplete)
+                {
+                    ChooseNewSection();
+                }
+                break;
+            }
+            
+            UpdateDestination();
+        }
+    }
+    
+    void UpdateDestination()
+    {
+        _navMeshAgent.SetDestination(_dest);
+    }
+}
---
+++ Proj_Chaos/Assets/Scripts/Characters/Slapper.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(PickUp))]
+public class Slapper : MonoBehaviour
+{
+    public Events.EventCharacterSlapped OnCharacterSlapped;
+    public Events.EventCharacterAbleSlap OnCharacterEndSlap;
+    
+    [SerializeField] private float radius;
+    [SerializeField] private float timeBetweenSlaps;
+    [SerializeField] private float timeIncapacitated;
+    [SerializeField] private float slapForce;
+    
+    [SerializeField] private bool canSlap = true;
+    private Rigidbody _rb;
+    private PickUp _pickUp;
+
+    private void Start()
+    {
+        _pickUp = GetComponent<PickUp>();
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    public void OnSlapPressed()
+    {
+        if (canSlap)
+        {
+            Slap();
+        }
+    }
+
+    private void Slap()
+    {
+        //should be on anim
+        StartCoroutine(DidSlap());
+        
+        Collider[] overlappedObjects = Physics.OverlapSphere(transform.position, radius);
+
+        foreach (Collider col in overlappedObjects)
+        {
+            Slapper slapper = col.GetComponent<Slapper>();
+            
+            if (slapper != null && slapper != this)
+            {
+                StartCoroutine(slapper.Slapped(col.transform.position - transform.position));
+            }
+        }
+    }
+
+    private IEnumerator DidSlap()
+    {
+        ChangeBoolStates(false);
+        
+        yield return new WaitForSeconds(timeBetweenSlaps);
+
+        ChangeBoolStates(true);
+    }
+
+    public void ChangeSlapBool(bool canSlap)
+    {
+        this.canSlap = canSlap;
+    }
+    
+    private IEnumerator Slapped(Vector3 dir)
+    {
+        ChangeBoolStates(false);
+        OnCharacterSlapped.Invoke();
+        
+        //Physics
+        _rb.isKinematic = false;
+        _rb.AddForce(dir.normalized * slapForce, ForceMode.Impulse);
+        
+        yield return new WaitForSeconds(timeIncapacitated);
+        
+        OnCharacterEndSlap.Invoke();
+        
+        //Physics
+        _rb.isKinematic = true;
+        
+        ChangeBoolStates(true);
+    }
+
+    private void ChangeBoolStates(bool canDo)
+    {
+        ChangeSlapBool(canDo);
+        _pickUp.ChangePickupBool(canDo);
+    }
+}
